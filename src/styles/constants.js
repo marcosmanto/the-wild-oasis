c8e55e70fd004af4bf6aebe47e4ec9ee92b625a8
Marcos Mantovani <mantovani.marcos@gmail.com>
2025-02-23 17:05:29 -0300

    create global styles ans variables and `constants.js` translates these css vars to JS objects for code completion inside `styled` template literal.

---
+++ src/styles/constants.js
@@ -0,0 +1,49 @@
+export const colors = {
+  brand50: "var(--color-brand-50)",
+  brand100: "var(--color-brand-100)",
+  brand200: "var(--color-brand-200)",
+  brand500: "var(--color-brand-500)",
+  brand600: "var(--color-brand-600)",
+  brand700: "var(--color-brand-700)",
+  brand800: "var(--color-brand-800)",
+  brand900: "var(--color-brand-900)",
+  
+  grey0: "var(--color-grey-0)",
+  grey50: "var(--color-grey-50)",
+  grey100: "var(--color-grey-100)",
+  grey200: "var(--color-grey-200)",
+  grey300: "var(--color-grey-300)",
+  grey400: "var(--color-grey-400)",
+  grey500: "var(--color-grey-500)",
+  grey600: "var(--color-grey-600)",
+  grey700: "var(--color-grey-700)",
+  grey800: "var(--color-grey-800)",
+  grey900: "var(--color-grey-900)",
+  
+  blue100: "var(--color-blue-100)",
+  blue700: "var(--color-blue-700)",
+  green100: "var(--color-green-100)",
+  green700: "var(--color-green-700)",
+  yellow100: "var(--color-yellow-100)",
+  yellow700: "var(--color-yellow-700)",
+  silver100: "var(--color-silver-100)",
+  silver700: "var(--color-silver-700)",
+  indigo100: "var(--color-indigo-100)",
+  indigo700: "var(--color-indigo-700)",
+  red100: "var(--color-red-100)",
+  red700: "var(--color-red-700)",
+  red800: "var(--color-red-800)",
+}
+
+export const shadows = {
+  sm: "var(--shadow-sm)",
+  md: "var(--shadow-md)",
+  lg: "var(--shadow-lg)",
+}
+
+export const borderRadius = {
+  tiny: "var(--border-radius-tiny)",
+  sm: "var(--border-radius-sm)",
+  md: "var(--border-radius-md)",
+  lg: "var(--border-radius-lg)",
+}
