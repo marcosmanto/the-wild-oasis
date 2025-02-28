bbf9ead0ea8b9bd8e10ef0abd3d0bcf06c3dadc7
Marcos Mantovani <mantovani.marcos@gmail.com>
2025-02-28 18:07:35 -0300

    add toast notifications and improve app's layout

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,12 @@ import Login from '@/pages/Login'
 import PageNotFound from '@/pages/PageNotFound'
 import Settings from '@/pages/Settings'
 import NewUsers from '@/pages/Users'
+import { colors } from '@/styles/constants'
 import GlobalStyles from '@/styles/GlobalStyles'
 import AppLayout from '@/ui/AppLayout'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { Toaster } from 'react-hot-toast'
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 
 const queryClient = new QueryClient({
@@ -24,6 +26,7 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools initialIsOpen={false} />
+
       <GlobalStyles />
       <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
         <Routes>
@@ -40,6 +43,27 @@ function App() {
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
+
+      <Toaster
+        position="top-center"
+        gutter={12}
+        containerStyle={{ margin: '8px' }}
+        toastOptions={{
+          success: {
+            duration: 3000
+          },
+          error: {
+            duration: 5000
+          },
+          style: {
+            fontSize: '16px',
+            maxWidth: '500px',
+            padding: '16px 24px',
+            backgroundColor: colors['grey-0'],
+            color: colors['grey-700']
+          }
+        }}
+      />
     </QueryClientProvider>
   )
 }
--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -2,6 +2,7 @@ import { deleteCabin } from '@/services/apiCabins'
 import { colors } from '@/styles/constants'
 import { formatCurrency } from '@/utils/helpers'
 import { useMutation, useQueryClient } from '@tanstack/react-query'
+import toast from 'react-hot-toast'
 import styled from 'styled-components'
 
 const TableRow = styled.div`
@@ -52,9 +53,9 @@ function CabinRow({ cabin }) {
     mutationFn: deleteCabin,
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['cabins'] })
-      alert('Cabin successfully deleted')
+      toast.success('Cabin successfully deleted')
     },
-    onError: err => alert(err.message)
+    onError: err => toast.error(err.message)
   })
 
   return (
