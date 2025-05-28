87bed0f2bdf39fe0e51e0e9474ecf0a1f5d2c3e9
Marcos Mantovani <mantovani.marcos@gmail.com>
2025-05-28 00:02:03 -0300

    feat: display active filter in cabins page heading
    - Add dynamic filter label display in page title
    - Show current discount filter status in parentheses
    - Create SmallText styled component for filter indicator
    - Integrate with URL search params to read active filter

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -3,12 +3,29 @@ import Row from '@/ui/Row'
 import CabinTable from '@/features/cabins/CabinTable'
 import AddCabin from '@/features/cabins/AddCabin'
 import CabinTableOperations from '@/features/cabins/CabinTableOperations'
+import { useSearchParams } from 'react-router-dom'
+import { options } from '@/features/cabins/cabinFilterOptions'
+
+import styled from 'styled-components'
+import { colors } from '@/styles/constants'
+
+const SmallText = styled.span`
+  font-size: 1.4rem; // Tamanho menor de fonte
+  color: ${colors['grey-400']}; // Cinza mais escuro
+  font-weight: 400; // Opcional: peso da fonte
+`
 
 function Cabins() {
+  const [searchParams] = useSearchParams()
+  const filterValue = searchParams.get('discount')
+  const selectedFilterLabel = options.find(option => option.value === filterValue)?.label?.toLocaleLowerCase()
+
   return (
     <>
       <Row type="horizontal" horizontalalign="space-between">
-        <Heading as="h1">All cabins</Heading>
+        <Heading as="h1">
+          All cabins <SmallText>{selectedFilterLabel ? '(' + selectedFilterLabel + ')' : ''}</SmallText>
+        </Heading>
         <CabinTableOperations></CabinTableOperations>
       </Row>
       <Row>
